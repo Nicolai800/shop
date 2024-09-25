96b4c92671886ccde45a90ab943219dd5d5e4f22
Nicolai.Parshutin <happy454420@gmail.com>
2024-09-25 18:13:14 +0200

    strip in process

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,8 +1,26 @@
 import { NextResponse } from "next/server"
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
-export const GET = ()=> {
+export const POST =  async ()=> {
+
+    const stripeProducts = [ {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: '{{PRICE_ID}}',
+            quantity: 1,
+          },]
+
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+        line_items: stripeProducts,
+        mode: 'payment',
+        success_url: ``,
+        cancel_url: ``,
+      });
     return NextResponse.json({
         data: 'Hellow world!'
     })
 }
+
+
+
